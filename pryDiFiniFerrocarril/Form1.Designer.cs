387af81e74bb7bac18b071ea82a2b790687e0c38
zairadifni <zairadifini123@gmail.com>
2025-09-17 13:41:02 -0300

    Agregar archivos de proyecto.

---
+++ pryDiFiniFerrocarril/Form1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace pryDiFiniFerrocarril
+{
+    partial class FrmFerrocaril
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FrmFerrocaril
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(491, 356);
+            Name = "FrmFerrocaril";
+            Text = "Calcular boletos ferrocarril";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ pryDiFiniFerrocarril/Form1.cs
@@ -0,0 +1,10 @@
+namespace pryDiFiniFerrocarril
+{
+    public partial class FrmFerrocaril : Form
+    {
+        public FrmFerrocaril()
+        {
+            InitializeComponent();
+        }
+    }
+}
