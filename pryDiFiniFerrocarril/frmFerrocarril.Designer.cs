1e7d79bec96e5bb6583dc53f3449931a1207d663
zairadifini <zairadifini123@gmail.com>
2025-09-17 18:36:48 -0300

    terminado

--- pryDiFiniFerrocarril/frmFerrocarril.Designer.cs
+++ pryDiFiniFerrocarril/frmFerrocarril.Designer.cs
@@ -31,55 +31,81 @@
             lblDistancia = new Label();
             txtDistancia = new TextBox();
             lblDias = new Label();
-            txtDias = new TextBox();
+            dudDias = new DomainUpDown();
+            lblResultado = new Label();
+            btnCalcular = new Button();
             SuspendLayout();
             // 
             // lblDistancia
             // 
             lblDistancia.AutoSize = true;
             lblDistancia.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblDistancia.Location = new Point(32, 51);
+            lblDistancia.Location = new Point(37, 68);
             lblDistancia.Name = "lblDistancia";
-            lblDistancia.Size = new Size(91, 25);
+            lblDistancia.Size = new Size(113, 32);
             lblDistancia.TabIndex = 0;
             lblDistancia.Text = "Distancia";
             // 
             // txtDistancia
             // 
-            txtDistancia.Location = new Point(129, 53);
+            txtDistancia.Location = new Point(147, 71);
+            txtDistancia.Margin = new Padding(3, 4, 3, 4);
             txtDistancia.Name = "txtDistancia";
-            txtDistancia.Size = new Size(189, 23);
+            txtDistancia.Size = new Size(215, 27);
             txtDistancia.TabIndex = 1;
             // 
             // lblDias
             // 
             lblDias.AutoSize = true;
             lblDias.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblDias.Location = new Point(32, 124);
+            lblDias.Location = new Point(37, 130);
             lblDias.Name = "lblDias";
-            lblDias.Size = new Size(154, 25);
+            lblDias.Size = new Size(196, 32);
             lblDias.TabIndex = 2;
             lblDias.Text = "Cantidad de dias";
             // 
-            // txtDias
+            // dudDias
             // 
-            txtDias.Location = new Point(192, 124);
-            txtDias.Name = "txtDias";
-            txtDias.Size = new Size(98, 23);
-            txtDias.TabIndex = 3;
+            dudDias.Location = new Point(239, 135);
+            dudDias.Name = "dudDias";
+            dudDias.Size = new Size(123, 27);
+            dudDias.TabIndex = 3;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(37, 263);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(75, 20);
+            lblResultado.TabIndex = 4;
+            lblResultado.Text = "Resultado";
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(268, 188);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(94, 29);
+            btnCalcular.TabIndex = 5;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
             // 
             // FrmFerrocaril
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(491, 356);
-            Controls.Add(txtDias);
+            ClientSize = new Size(438, 475);
+            Controls.Add(btnCalcular);
+            Controls.Add(lblResultado);
+            Controls.Add(dudDias);
             Controls.Add(lblDias);
             Controls.Add(txtDistancia);
             Controls.Add(lblDistancia);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmFerrocaril";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Calcular boletos ferrocarril";
+            Load += FrmFerrocaril_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -89,6 +115,8 @@
         private Label lblDistancia;
         private TextBox txtDistancia;
         private Label lblDias;
-        private TextBox txtDias;
+        private DomainUpDown dudDias;
+        private Label lblResultado;
+        private Button btnCalcular;
     }
 }
--- pryDiFiniFerrocarril/frmFerrocarril.cs
+++ pryDiFiniFerrocarril/frmFerrocarril.cs
@@ -2,9 +2,52 @@ namespace pryDiFiniFerrocarril
 {
     public partial class FrmFerrocaril : Form
     {
+        decimal Kilometro = 5m;
+        decimal PrecioIda;
+        decimal Distancia;    
+        int Dia;             
+        decimal PrecioFinal;
+        decimal PrecioTotal;
+        decimal TotalRecorrer;
         public FrmFerrocaril()
         {
             InitializeComponent();
+        }
+
+        private void FrmFerrocaril_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 30; i++)
+            {
+                dudDias.Items.Add(i);
+            }
+
+            // Opcional: que arranque mostrando el primer valor
+            dudDias.SelectedIndex = 0;
+
+            PrecioIda = 0m;
+            PrecioTotal = 0m;
+            TotalRecorrer = 0m;
+            PrecioFinal = 0m;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Distancia = Convert.ToDecimal(txtDistancia.Text);
+            Dia = Convert.ToInt32(dudDias.SelectedItem);
+
+
+            PrecioIda = Kilometro * Distancia;
+            PrecioTotal = PrecioIda * 2;
+            PrecioFinal = PrecioTotal; 
+
+            if (Dia >= 7 && Distancia >= 100)
+            {
+                PrecioFinal = PrecioTotal * 0.50m; 
+            }
+
+            lblResultado.Text = "Precio Final: $" + PrecioFinal.ToString("0.00");
+
+
         }
     }
 }
